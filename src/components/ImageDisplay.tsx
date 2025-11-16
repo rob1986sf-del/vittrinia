8d98acb013d9adfc2bf6ce7314d6110bc84217a8
robsonferrari <rob1986sf@gmail.com>
2025-11-16 06:45:17 -0300

    Update ImageDisplay.tsx

--- src/components/ImageDisplay.tsx
+++ src/components/ImageDisplay.tsx
@@ -1,1 +1,21 @@
+import React from "react";
+
+export default function ImageDisplay({ image }) {
+  if (!image)
+    return (
+      <p style={{ textAlign: "center", marginTop: "20px" }}>
+        Sua imagem aparecerá aqui...
+      </p>
+    );
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <img
+        src={image}
+        alt="Imagem gerada"
+        style={{ maxWidth: "100%", borderRadius: "10px" }}
+      />
+    </div>
+  );
+}
 
